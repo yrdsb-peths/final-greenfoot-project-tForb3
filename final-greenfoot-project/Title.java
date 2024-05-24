3daba65f7bd08a0ee11fa08f28b24a6aae343b45
tForb3 <349950253@gapps.yrdsb.ca>
2024-05-24 13:54:42 -0400

    Created outline

---
+++ final-greenfoot-project/Title.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Title here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Title extends MyWorld
+{
+
+    /**
+     * Constructor for objects of class Title.
+     * 
+     */
+    public Title()
+    {
+    }
+}
